82241cc6930563db0af00931971fd99560ce80ff
Aditya Rana <ranaadi745@gmail.com>
2025-08-04 16:30:58 +0530

    Add files via upload

---
+++ Greet.py
@@ -0,0 +1,9 @@
+def greeting(name):
+    print("Hello, " + name )
+greeting("Aditya Rana")
+
+def add():
+   a = int(input("Enter first number: "))
+   b = int(input("Enter second number: "))
+   return a + b
+print("The sum is:", add())  # Call the function to see the result  
